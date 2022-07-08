f6f9566a7cfff094993efb415beab0e1e2fab809
Moss <danielsmoss96@gmail.com>
2022-07-08 12:12:22 +0100

    Very basic integration with Steam Rolled API to get random game each time

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-const testFunk = () => {
-    fetch('https://steam-rolled.herokuapp.com/api')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
+//DOM cache
+const title = document.getElementById('gametitle')
+const mov = document.getElementById('mov')
+const img1 = document.getElementById('img1')
+const img2 = document.getElementById('img2')
+const img3 = document.getElementById('img3')
+const tags = document.getElementById('tags')
+const lang = document.getElementById('languages')
+const bio = document.getElementById('bio')
+const developer = document.getElementById('dev')
+const publisher = document.getElementById('publisher')
+
+let data
+
+const initialiseData = async () => {
+    const response = await fetch('https://steam-rolled.herokuapp.com/api/games')
+    data = await response.json()
+    assignDOM(data[Math.floor(Math.random() * data.length)])
 }
 
-testFunk()
+const randomGame = () => {
+    assignDOM(data[Math.floor(Math.random() * data.length)])
+}
+
+const assignDOM = ({game_title, links: [links], genres, languages, description, details, app_id}) => {
+    console.log(game_title, links, genres, languages, description, details, app_id)
+
+    title.textContent = game_title
+    bio.textContent = description
+    developer.textContent = details.Developer
+    publisher.textContent = details.Publisher
+    mov.src = links[0]
+    img1.src = links[1]
+    img2.src = links[2]
+    img3.src = links[3]
+
+    let genreHTML = `<li><strong>GENRES</strong></li>
+    `
+    genres.forEach(genre => {
+        genreHTML += `<li><em>${genre}</em></li>
+        `
+    })
+    tags.innerHTML = genreHTML
+
+    let languagesHTML = `<li><strong>LANGUAGES</strong></li>
+    `
+    languages.forEach(language => {
+        languagesHTML += `<li><em>${language}</em></li>
+        `
+    })
+    lang.innerHTML = languagesHTML
+}
+
+
+initialiseData()
