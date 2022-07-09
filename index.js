e1762a81057af9a943905855f8a3a1b9d0f83400
Moss <danielsmoss96@gmail.com>
2022-07-09 17:31:55 +0100

    Implement very basic system to show matches

--- index.js
+++ index.js
@@ -9,40 +9,78 @@ const lang = document.getElementById('languages')
 const bio = document.getElementById('bio')
 const developer = document.getElementById('dev')
 const publisher = document.getElementById('publisher')
+
+const swipeButton = document.getElementById('swipe')
 const swipeSection = document.getElementById('swipesection')
+const matchesButton = document.getElementById('matches')
+const matchesSection = document.getElementById('matchessection')
+const matchlist = document.getElementById('matchlist')
+const seenArray = []
 
 const yes = document.getElementById('yes')
 const no = document.getElementById('no')
 
 let data
+let currentGame
 
 yes.addEventListener('click', () => {
-    //add to likes
-    swipeSection.scrollTop = 0
-    randomGame()
+    addMatch(currentGame)
+    loadNext()
 })
 
-no.addEventListener('click', () => {
-    //ignore and move on
+no.addEventListener('click', () => {loadNext()})
+
+
+
+swipeButton.addEventListener('click', function() {loadSwipe()})
+matchesButton.addEventListener('click', function() {loadMatches()})
+
+function loadSwipe() {
+    swipeSection.style.display = 'block'
+    matchesSection.style.display = 'none'
+    yes.style.display = 'block'
+    no.style.display = 'block'
+}
+
+function loadMatches() {
+    swipeSection.style.display = 'none'
+    matchesSection.style.display = 'block'
+    yes.style.display = 'none'
+    no.style.display = 'none'
+}
+
+function loadNext() {
     swipeSection.scrollTop = 0
     randomGame()
-})
+}
 
-
+function addMatch(game) {
+    matchlist.innerHTML += `<li>
+    ${game.game_title} - <a href="https://store.steampowered.com/app/${game.app_id}">View on Steam</a>
+    </li>
+    <hr>`
+}
 
 const initialiseData = async () => {
     const response = await fetch('https://steam-rolled.herokuapp.com/api/games')
     data = await response.json()
-    assignDOM(data[Math.floor(Math.random() * data.length)])
+    currentGame = data[Math.floor(Math.random() * data.length)]
+    while (seenArray.includes(currentGame)) currentGame = data[Math.floor(Math.random() * data.length)]
+    seenArray.push(currentGame)
+    assignDOM(currentGame)
 }
 
 const randomGame = () => {
-    assignDOM(data[Math.floor(Math.random() * data.length)])
+    currentGame = data[Math.floor(Math.random() * data.length)]
+    while (seenArray.includes(currentGame)) currentGame = data[Math.floor(Math.random() * data.length)]
+    seenArray.push(currentGame)
+    assignDOM(currentGame)
+    assignDOM(currentGame)
+
+    console.log(seenArray)
 }
 
 const assignDOM = ({game_title, links: [links], genres, languages, description, details, app_id}) => {
-    console.log(game_title, links, genres, languages, description, details, app_id)
-
     title.textContent = game_title
     bio.textContent = description
     developer.textContent = details.Developer
@@ -57,7 +95,6 @@ const assignDOM = ({game_title, links: [links], genres, languages, description, 
     }
     mov.innerHTML = `<source src="${movLink}"></source>`
     mov.load()
-    console.log(mov.innerHTML)
     img1.src = links[1]
     img2.src = links[2]
     img3.src = links[3]
